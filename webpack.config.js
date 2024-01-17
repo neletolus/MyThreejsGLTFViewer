09212704da2fe52c5916d0fb66fb95b2db80de13
neletolus <y1405705@gmail.com>
2024-01-17 22:27:26 +0900

    デザインの基本対応

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const MODE = "production";
+
+const enabledSourceMap = MODE === "development";
+
 module.exports = {
-  mode: "production",
+  mode: MODE,
 
   entry: "./src/index.ts",
   output: {
@@ -11,6 +15,30 @@ module.exports = {
       {
         test: /\.ts$/,
         use: "ts-loader",
+      },
+      {
+        test: /\.scss/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: enabledSourceMap
+            },
+          },
+        ],
       },
     ],
   },
